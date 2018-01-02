51d4ccb76c4fff28e76475d44b740459ff9059e6
WeiHung <whc1g10@gmail.com>
2018-01-02 23:13:28 +0800

    initial source

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id = "app" class = "container">
+   <h3>{{title}}</h3>
+
+   <div class = "col-md-12">
+
+   <Character 
+   v-for="(id, index) in initial_ids" 
+   v-bind:id = "id" 
+   :key="index"
+   />
+   </div>
+
+</div>
+</template>
+
+
+<script>
+
+import Character from './components/Character.vue';
+
+  export default{
+    name: 'app',
+    data(){
+      return{
+        title:'Generate Your Team',
+        initial_ids:[1,13,14]
+      }
+    },
+    components:{
+      Character
+    }
+  }
+</script>
+
+
+
