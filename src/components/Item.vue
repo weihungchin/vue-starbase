c1bd87ca018d3be918f48d3d1504bd7fc1cd0dbc
WeiHung <whc1g10@gmail.com>
2018-01-04 22:13:41 +0800

    call backend with router + do not modify props directly

---
+++ src/components/Item.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class = "col-md-4" @click = "switchItems">
+      <div class="item-card">
+          <div class="card-block">
+              <h4 class="card-title">
+                  {{item.name}}
+              </h4>
+              <div v-for = "(value, key, index) in item">
+                  <div v-if = "index <5">
+                      <strong>{{key}}</strong>: {{value}}
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props:['passedItem', 'type'],
+        data(){
+            return{
+                item:{}
+            }
+        },
+        methods:{
+            switchItems(){
+                let random_id = Math.floor(Math.random()*63)+1;
+                fetch(`https://swapi.co/api/${this.type}/${random_id}`, {
+                    method:"GET"
+                })
+                .then(res => res.json())
+                .then(json => this.item = json);
+
+            }
+        },
+        created(){
+            this.item = this.passedItem
+        }
+    }
+</script>
